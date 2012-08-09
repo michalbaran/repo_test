7b4a3b7415d7190009330dbf9d80af05cc39baf4
Michal <misho.baran@gmail.com>
2012-08-09 15:09:37 +0200

    moj prvy commit

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mcMath
+{
+    public class Class1
+    {
+        //constr
+        public Class1()
+        { 
+        }
+
+        public long Add(long val1, long val2)
+        {
+            return val1 + val2; 
+        }
+
+    }
+}
